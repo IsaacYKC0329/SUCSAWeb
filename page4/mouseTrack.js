40e3003626d86542bd7e2b83a68493e4f90acc56
“YuRubik” <2980045161@qq.com>
2023-03-03 16:14:09 +1100

    change chicken

--- page4/mouseTrack.js
+++ page4/mouseTrack.js
@@ -11,14 +11,14 @@ image.addEventListener("mousemove", function(event) {
     let x = event.offsetX;
     let y = event.offsetY;
     coordinates.innerHTML = "x=" + x + ", y=" + y;
-    if(x < 1360&& x>1325 && y>525 && y<555){
+    if(x < 715 && x>683 && y>300 && y<335){    //北京
         beijing.style.visibility = "visible";
     }else {
         beijing.style.visibility = "hidden";
     }
 
 
-    if(x > 1239&& x<1300 && y>535 && y<660){
+    if(x > 600 && x<665 && y>310 && y<435){    //山西
         shanxi.style.visibility = "visible";
     }else {
         shanxi.style.visibility = "hidden";
